7aae1076f84d033253c8f3d1fc7a5be790f1a7c8
laion12 <pandaerox@gmail.com>
2022-07-01 04:07:21 -0500

    menu adaptable e imagenes web incorporadas

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 import {quitar_cargador} from "./modulos/cargador.js";
-import {menu_movimiento} from "./modulos/menu.js";
 
 const cargador1=document.querySelector(".cargador_contenedor");
 window.addEventListener("load", ()=>{
     quitar_cargador(cargador1);
 });
 
-const main_scroll =document.getElementById('i_contenedor')
+const main_scroll =document.getElementById('i_contenedor');
 const menu =document.getElementById('menu');
-const lista =document.querySelectorAll('.menu_text');
-const ventana =window.innerHeight;
-var $head=document.querySelector(".i_head").getBoundingClientRect().bottom;
-//main_scroll.addEventListener("scroll",()=>{
-    //menu_movimiento(menu,lista,ventana,$head);
-//});
+main_scroll.addEventListener("scroll",()=>{
+    var $head=document.querySelector(".i_head").getBoundingClientRect().bottom;
+    menu.classList.toggle("animation_mov",$head<0);
+});
